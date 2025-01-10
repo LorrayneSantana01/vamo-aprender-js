7a84a0699f95c0a69de4a06984de388e50b86b92
lorrayne.santana <lorrayne.santana2022@gmail.com>
2025-01-10 12:14:21 -0300

    alterar html com js class

---
+++ 3-alterar-html-com-js-class/script.js
@@ -0,0 +1,6 @@
+// Alterar HTML com JS - ID
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragrafo = document.getElementById("paragrafo-html")
+    paragrafo.textContent = "Esse é um parágrafo alterado por JS"
+})
