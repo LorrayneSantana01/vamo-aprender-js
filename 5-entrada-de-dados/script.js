e6f0b64964533c90e1b2fd3565570f2e680ff688
lorrayne.santana <lorrayne.santana2022@gmail.com>
2025-01-10 12:20:37 -0300

    entrada de dados

---
+++ 5-entrada-de-dados/script.js
@@ -0,0 +1,9 @@
+// Entrada de dados
+
+const idade = prompt("Qual sua idade?")
+
+if (idade < 13) {
+    alert("Esse site não é recomendado para menores de 13 anos")
+} else {
+    document.write("Seja bem vindo(a)!")
+}
