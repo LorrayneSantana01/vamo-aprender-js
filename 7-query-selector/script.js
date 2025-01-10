d8544fd78186b14e9b099d6e13c961a73140c59a
lorrayne.santana <lorrayne.santana2022@gmail.com>
2025-01-10 14:23:17 -0300

    query selector

---
+++ 7-query-selector/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const paragrafo1 = document.querySelector("#meu-paragrafo")
+    paragrafo1.textContent = "meu ID"
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragrafo2 = document.querySelector(".meu-paragrafo")
+    paragrafo2.textContent = "minha classe"
+})
