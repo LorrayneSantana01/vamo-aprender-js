4bce26d4408d7f2ad8e7c49150144a69320423c3
LorrayneSantana01 <lorrayne.santana2022@gmail.com>
2025-01-18 10:32:52 -0300

    alterando conteudo html por acao

---
+++ 10-alterar-conteudo-html-por-acao/script.js
@@ -0,0 +1,9 @@
+const meuLorem = document.getElementById("lorem")
+
+const button = document.getElementById("botao")
+
+button.addEventListener('click',
+    function() {
+        meuLorem.innerHTML = "<b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, alias.</b"
+    }
+)
