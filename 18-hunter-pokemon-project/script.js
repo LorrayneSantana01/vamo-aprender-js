fcb9727d386dd7a75edbed0d1362a97a1571df25
lorrayne.santana <lorrayne.santana2022@gmail.com>
2025-01-20 17:44:53 -0300

    inicio project hunter pokemon

--- 18-hunter-pokemon-project/script.js
+++ 18-hunter-pokemon-project/script.js
@@ -1,1 +1,20 @@
-console.log("Oii")
+window.addEventListener('DOMContentLoaded', function() {
+
+    const body = document.querySelector("body")
+    const game = document.querySelector(".game")
+    
+    const count = document.querySelector("h1")
+    const reset = document.querySelector("reset")
+    
+    const ash = document.querySelector("#ash")
+    
+    const zubat = document.querySelector("#zubat")
+    const pikachu = document.querySelector("#pikachu")
+    const charmander = document.querySelector("#charmander")
+    
+    body.addEventListener('keydown', (event) => {
+        event.stopPropagation()
+    
+        console.log(event.code)
+    })
+})
